9975003b854c6d56d5a3015ac5b852adeefebc3f
Frank Marra <frankrmarra@gmail.com>
2022-11-21 13:54:53 -0500

    added sign in link to about page

--- backlog2/pages/about.js
+++ backlog2/pages/about.js
@@ -1,15 +1,19 @@
 import styles from '../styles/AboutPage.module.css'
+import Nav from '../components/Nav'
 
 const About = () => {
   return (
-    <div className={styles['about']}>
-      <h1>Welcome to Backlog! </h1>
-      <p>
-        Search for a game to add to your backlog and start your quest to have
-        zero games left to play! Yeah, we know that's impossible, but maybe
-        chipping away at it will make you feel better!
-      </p>
-    </div>
+    <>
+      <Nav />
+      <div className={styles['about']}>
+        <h1>Welcome to Backlog! </h1>
+        <p>
+          Search for a game to add to your backlog and start your quest to have
+          zero games left to play! Yeah, we know that's impossible, but maybe
+          chipping away at it will make you feel better!
+        </p>
+      </div>
+    </>
   )
 }
 
